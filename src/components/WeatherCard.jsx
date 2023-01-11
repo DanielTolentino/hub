994bee6946a2bb99493d4b5cf1187de44ed14d3e
DanielTolentino <danieltolentino@protonmail.com>
2023-01-11 15:45:07 -0300

    change to main temp from API

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,10 +1,11 @@
 import 'solid-js';
+import "../Styles/WeatherCard.css"
 
 const api_key = import.meta.env.SECRET_KEY;
 const queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=-19.4658&lon=-44.2467" + "&appid=" + api_key +"&lang=pt_br&units=metric";
 const response = await fetch(queryURL);
 const data = await response.json();
-const feel_weather = data.main.feels_like;
+const feel_weather = data.main.temp;
 const humidity = data.main.humidity;
 const wind = data.wind.speed;
 const desc = data.weather[0].description;
@@ -14,12 +15,12 @@ function HelloWorld() {
   return <li class="weather-card">
             <h1>Sete Lagoas </h1>
             <h2>
-                <span>🌦️{feel_weather}°C</span>
-                <span>({desc})</span>
+                <span>🌦️{feel_weather}°C </span>
+                <span>({desc}) </span>
                 |
-                <span>💧{humidity}%</span>
+                <span> 💧{humidity}% </span>
                 |
-                <span>🍃{wind}m/s</span>
+                <span> 🍃{wind}m/s</span>
           </h2>
         </li>;
 }

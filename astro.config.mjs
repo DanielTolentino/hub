613859d40855407c07796d449240f99119102b0a
Daniel <danieltolentino@protonmail.com>
2023-01-25 00:10:47 -0300

    remove netlify functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,6 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
